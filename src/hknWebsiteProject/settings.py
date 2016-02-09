b72ebb86258beb86fb495bc2ca8c1034e3508492
nimmerman <immerman@umich.edu>
2016-02-09 15:22:07 -0500

    email setting

--- src/hknWebsiteProject/settings.py
+++ src/hknWebsiteProject/settings.py
@@ -176,6 +176,8 @@ EMAIL_HOST_USER = 'hkn.website@gmail.com'
 EMAIL_HOST_PASSWORD = 'hknwebsite'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 
 
 
