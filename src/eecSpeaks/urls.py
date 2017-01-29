2fd87cdaba3bce961b93609afe0af89bd334a480
Gabe Hodge <gdhodge@umich.edu>
2017-01-29 14:35:14 -0500

    Updated name of post page and added a link to it from the EECS speaks homepage

--- src/eecSpeaks/urls.py
+++ src/eecSpeaks/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^blog/(?P<blogid>[0-9]*)$', views.view_post),
-    url(r'^addblogpost', views.add_blogpost, name='addblogpost'),
+    url(r'^post', views.add_blogpost, name='post'),
     url(r'', views.show_blogposts, name='eecspeaks')
 ]
