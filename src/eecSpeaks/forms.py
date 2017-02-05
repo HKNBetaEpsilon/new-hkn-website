bdb7341bca3e0fa107e2af0a70077fb13048d939
Gabe Hodge <gdhodge@umich.edu>
2017-02-05 16:20:50 -0500

    incorporated form formatting to edit page and cleaned up space/tab inconsistency

--- src/eecSpeaks/forms.py
+++ src/eecSpeaks/forms.py
@@ -10,10 +10,10 @@ class BlogForm(forms.ModelForm):
         model = Blog
         exclude = ['publicationDate', ]
     def __init__(self, *args, **kwargs):
-    	self.helper = FormHelper()
-    	self.helper.form_id = 'Blog Form'
-    	self.helper.form_class = 'blog-form'
-    	self.helper.form_method = 'post'
-    	self.helper.form_action = ''
-    	self.helper.add_input(Submit('submit', 'Submit'))
-    	super(BlogForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'Blog Form'
+        self.helper.form_class = 'blog-form'
+        self.helper.form_method = 'post'
+        self.helper.form_action = ''
+        self.helper.add_input(Submit('submit', 'Submit'))
+        super(BlogForm, self).__init__(*args, **kwargs)
