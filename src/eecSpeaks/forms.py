7d0d26ffa15b7afe9beefc4624d8437035b8c5d7
Amir Refai <amirrefai.1@gmail.com>
2017-02-05 16:12:39 -0500

    Added css styling to add blog post form

--- src/eecSpeaks/forms.py
+++ src/eecSpeaks/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 
 from .models import Blog
 
@@ -6,3 +9,11 @@ class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         exclude = ['publicationDate', ]
+    def __init__(self, *args, **kwargs):
+    	self.helper = FormHelper()
+    	self.helper.form_id = 'Blog Form'
+    	self.helper.form_class = 'blog-form'
+    	self.helper.form_method = 'post'
+    	self.helper.form_action = ''
+    	self.helper.add_input(Submit('submit', 'Submit'))
+    	super(BlogForm, self).__init__(*args, **kwargs)
