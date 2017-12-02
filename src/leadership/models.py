73d60fab5054291586c906d09cf6448af5a7e4af
Henry Ellis <ellish@umich.edu>
2017-12-01 19:03:09 -0500

    added ability to give officers descriptions

--- src/leadership/models.py
+++ src/leadership/models.py
@@ -18,6 +18,7 @@ class Leader(models.Model):
     member = models.ForeignKey(Member, null=True, blank=True, on_delete=models.SET_NULL)
     position_type = models.CharField(max_length=1, choices=POSITION_TYPE, default='C')
     email = models.CharField(max_length=100, default='hkn-officers@umich.edu')
+    description = models.CharField(max_length=140, default='Hi I\'m an officer')
     display_order = models.IntegerField(default=0)
 
     def __unicode__(self):
